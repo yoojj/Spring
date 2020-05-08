c11b9f596692976ba09106240b82dfb2ea5b8cfd
yjj <yoojj.xyz@gmail.com>
2020-05-09 06:30:04 +0900

    MySqlConnectionTest

---
+++ springFramework/src/test/java/ex/spring/framework/MySqlConnectionTest.java
@@ -0,0 +1,34 @@
+package ex.spring.framework;
+
+import java.sql.Connection;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:config/root-context.xml", "classpath:config/app/db-context.xml"})
+@WebAppConfiguration
+public class MySqlConnectionTest {
+
+	@Autowired
+	private DataSource ds;
+
+	@Test
+	public void test() throws Exception {
+
+		try(Connection conn = ds.getConnection()){ 
+			System.out.println(conn);
+			
+		} catch (Exception e) { 
+			e.printStackTrace();
+		}
+		
+	}
+	
+}
