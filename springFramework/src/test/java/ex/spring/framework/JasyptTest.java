9e6e72b2794a46110ca505873400e2ffdbd9a6b1
yjj <yoojj.xyz@gmail.com>
2020-05-09 07:01:34 +0900

    JasyptTest

---
+++ springFramework/src/test/java/ex/spring/framework/JasyptTest.java
@@ -0,0 +1,29 @@
+package ex.spring.framework;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:config/root-context.xml", "classpath:config/app/db-context.xml"})
+@WebAppConfiguration
+public class JasyptTest {
+
+	@Test
+	public void test() {
+		
+		final StandardPBEStringEncryptor jastypt = new StandardPBEStringEncryptor();
+	
+		jastypt.setAlgorithm("PBEWithMD5AndDES");
+		jastypt.setPassword("key");
+		
+		String password = jastypt.encrypt("password");
+		
+		System.out.println(password);
+		
+	}
+	
+}
