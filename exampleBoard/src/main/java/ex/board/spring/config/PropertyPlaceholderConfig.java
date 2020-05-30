63525801c2ad6be1e090413eb651b1e0f00eca54
yjj <yoojj.xyz@gmail.com>
2020-05-31 07:51:48 +0900

    PropertyPlaceholderConfig

---
+++ exampleBoard/src/main/java/ex/board/spring/config/PropertyPlaceholderConfig.java
@@ -0,0 +1,22 @@
+package ex.board.spring.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.io.ClassPathResource;
+
+@Configuration
+public class PropertyPlaceholderConfig {
+
+	@Bean
+	public static PropertySourcesPlaceholderConfigurer placeholderConfigurer() {
+
+		final PropertySourcesPlaceholderConfigurer pspc = new PropertySourcesPlaceholderConfigurer();
+		
+		pspc.setLocations(new ClassPathResource("properties/db.properties"));
+		pspc.setIgnoreUnresolvablePlaceholders(true);
+		
+		return pspc;
+	}
+	
+}
