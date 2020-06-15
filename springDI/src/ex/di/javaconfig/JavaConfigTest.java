b5cc24e58278bbe15c58812c1b45c619aec14a2a
yjj <yoojj.xyz@gmail.com>
2020-06-15 21:53:50 +0900

    JavaConfigTest

--- springDI/src/ex/di/javaconfig/JavaConfigTest.java
+++ springDI/src/ex/di/javaconfig/JavaConfigTest.java
@@ -1,33 +1,48 @@
 package ex.di.javaconfig;
 
 import org.junit.Test;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.AbstractApplicationContext;
 
+import ex.bean.Board;
 import ex.bean.BoardService;
 import ex.bean.BoardServiceImpl;
-
 
 @Configuration
 public class JavaConfigTest {
 
-	@Bean(name="service")
+	@Bean
+	public String getContent() {
+		return "content";
+	}
+	
+	@Bean(name="boardBean")
+	public Board getBoard() {
+		return new Board(1, "javaConfig", getContent());	
+	}
+	
+	@Bean(name="serviceBean")
 	public BoardService getService() {
 		return new BoardServiceImpl();
 	}
 	
-	@SuppressWarnings("resource")
 	@Test
 	public void test() {
 		
-		final ApplicationContext context = new AnnotationConfigApplicationContext(JavaConfigTest.class);
+		final AbstractApplicationContext context = new AnnotationConfigApplicationContext(JavaConfigTest.class);
 		
-		final BoardService service = (BoardService) context.getBean("service");
+		final Board board = (Board) context.getBean("boardBean");
+		
+		final BoardService service = (BoardService) context.getBean("serviceBean");
+		
+		service.insert(board);
 		
 		System.out.println(service.select().toString());
 
+		context.close();
+		
 	}
 	
 }
