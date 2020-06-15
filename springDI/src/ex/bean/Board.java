65604294dcfc0e32691d36ec28a2305b6932bf87
yjj <yoojj.xyz@gmail.com>
2020-06-15 18:00:31 +0900

    Board

--- springDI/src/ex/bean/Board.java
+++ springDI/src/ex/bean/Board.java
@@ -9,6 +9,14 @@ public class Board implements Serializable {
 	private String title;
 	private String content;
 	
+	public Board() {}
+
+	public Board(int no, String title, String content) {
+		this.no = no;
+		this.title = title;
+		this.content = content;
+	}
+	
 	public int getNo() {
 		return no;
 	}
