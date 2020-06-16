f80799bb5a8585efa80d87f181c7c9fed840f754
yjj <yoojj.xyz@gmail.com>
2020-06-17 02:23:14 +0900

    ExceptionController

---
+++ exampleBoard/src/main/java/ex/board/spring/common/exception/ExceptionController.java
@@ -0,0 +1,30 @@
+package ex.board.spring.common.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.servlet.NoHandlerFoundException;
+
+@ControllerAdvice
+public class ExceptionController {
+
+	@ExceptionHandler(NoHandlerFoundException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public String error404(NoHandlerFoundException e, Model model) {
+
+		model.addAttribute("error", "404");
+		
+		return "/404";
+	}
+
+	@ExceptionHandler(PageNotFoundException.class)
+	public String pageNotFoundException(PageNotFoundException e, Model model) {
+
+		model.addAttribute("error", e.getMessage());
+
+		return "404";
+	}
+	
+}
